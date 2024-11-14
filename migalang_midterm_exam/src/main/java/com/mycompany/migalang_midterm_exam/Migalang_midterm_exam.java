69c38b30be658d4f78822cc80fe9045988176e7f
Tiger-alpha <160233934+Tiger-alpha@users.noreply.github.com>
2024-11-14 10:20:52 +0800

    Add files via upload

---
+++ migalang_midterm_exam/src/main/java/com/mycompany/migalang_midterm_exam/Migalang_midterm_exam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.migalang_midterm_exam;
+
+/**
+ *
+ * @author CL3-PC22
+ */
+public class Migalang_midterm_exam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
